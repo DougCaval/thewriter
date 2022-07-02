ba6ba7c5d908ca2e69d43c602198868db2658639
dougcaval <dougcaval.reis@gmail.com>
2022-07-02 12:59:23 -0300

    heroku config

--- thewriter_backend/src/config/database.js
+++ thewriter_backend/src/config/database.js
@@ -2,6 +2,8 @@ const { default: mongoose } = require("mongoose");
 
 
 const url = 'mongodb://localhost:27017/thewriter';
+//const url = 'mongodb+srv://cavalIt:<password>@cluster0.cbb4b.mongodb.net/?retryWrites=true&w=majority';
+
 mongoose.connect(url, {useNewUrlParser: true});
 
 module.exports = mongoose
