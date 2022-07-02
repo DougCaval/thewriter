e25dc446c7179987b60e92db75bdf4ddf99d74dc
dougcaval <dougcaval.reis@gmail.com>
2022-07-02 17:18:10 -0300

    database config 2

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 
 //const url = 'mongodb://localhost:27017/thewriter';
-const url = 'mongodb+srv://cavalIt:Caval2022@cluster0.cbb4b.mongodb.net/?retryWrites=true&w=majority';
+const url = 'mongodb+srv://cavalIt:Caval2022@cluster0.cbb4b.mongodb.net/?retryWrites=true&w=majority/thewriter/estorias';
 
 mongoose.connect(url, {useNewUrlParser: true});
 
