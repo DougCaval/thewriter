8c16fb97672e8dbaaba632fc4eae5e857eb8c4b6
dougcaval <dougcaval.reis@gmail.com>
2022-07-02 13:54:47 -0300

    config process

--- thewriter_backend/src/index.js
+++ thewriter_backend/src/index.js
@@ -8,6 +8,6 @@ server .use(cors());
 const TaskRoutes = require("./routes/TaskRoutes");
 server.use("/estorias", TaskRoutes);
 
-server.listen( proccess .env.PORT || 8080, () => {
+server.listen( process .env.PORT || 8080, () => {
   console.log(" Ther Writer are on line for you...");
 });
